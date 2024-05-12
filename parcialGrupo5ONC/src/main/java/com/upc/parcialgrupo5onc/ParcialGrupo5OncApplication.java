37328da9e71290ac5d53256f6f9bf0d00e4bd07c
zzzgrucho <89281221+zzzgrucho@users.noreply.github.com>
2024-05-11 21:00:13 -0500

    Add files via upload

---
+++ parcialGrupo5ONC/src/main/java/com/upc/parcialgrupo5onc/ParcialGrupo5OncApplication.java
@@ -0,0 +1,13 @@
+package com.upc.parcialgrupo5onc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialGrupo5OncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialGrupo5OncApplication.class, args);
+    }
+
+}
---
+++ parcialGrupo5ONC/src/test/java/com/upc/parcialgrupo5onc/ParcialGrupo5OncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.parcialgrupo5onc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialGrupo5OncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
